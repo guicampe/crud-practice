5fc51620c36432d0cad884e2bd9a029ee322482d
Guilherme Ramos Campestrini <gramoscampestrini@gmail.com>
2026-04-05 20:28:17 -0300

    fix: centralized onMounted

--- frontend/src/components/admin/User.vue
+++ frontend/src/components/admin/User.vue
@@ -1,12 +1,25 @@
 <script setup>
+import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+import { useUserById } from '@/composables/useUserById';
 import Back from '../layout/Back.vue';
 import UserPersonalInfo from '../ui/UserPersonalInfo.vue';
 
+const route = useRoute();
+const { userById, fetchUserById } = useUserById();
+
+onMounted(async () => {
+    await fetchUserById(route.params.id);
+})
 </script>
 
 <template>
     <main class="h-screen bg-indigo-100">
         <Back />
-        <UserPersonalInfo />
+        <UserPersonalInfo 
+            :name="userById?.name"
+            :id="userById?.id"
+            :email="userById?.email"
+        />
     </main>
 </template>
--- frontend/src/components/ui/UserPersonalInfo.vue
+++ frontend/src/components/ui/UserPersonalInfo.vue
@@ -1,14 +1,8 @@
 <script setup>
-import { onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { useUserById } from '@/composables/useUserById';
-
-
-const route = useRoute();
-const { userById, fetchUserById } = useUserById();
-
-onMounted(async () => {
-    await fetchUserById(route.params.id);
+defineProps({
+    name: String,
+    id: Number,
+    email: String
 })
 </script>
 
@@ -18,9 +12,9 @@ onMounted(async () => {
             <h1 class="text-gray-800 text-4xl font-bold mb-10 text-shadow-md">Área do aluno</h1>
             <div class="flex flex-col gap-1 border-2 border-indigo-400 p-2 bg-gray-200 rounded-md shadow-md">
                 <h2 class="text-gray-800 text-2xl font-bold text-shadow-md">Informações pessoais</h2>
-                <p class="text-indigo-500 font-bold">Nome: <span class="text-gray-800 font-normal">{{ userById?.name }}</span></p>
-                <p class="text-indigo-500 font-bold">RA: <span class="text-gray-800 font-normal">{{ userById?.id }}</span></p>
-                <p class="text-indigo-500 font-bold">E-mail: <span class="text-gray-800 font-normal">{{ userById?.email }}</span></p>
+                <p class="text-indigo-500 font-bold">Nome: <span class="text-gray-800 font-normal">{{ name }}</span></p>
+                <p class="text-indigo-500 font-bold">RA: <span class="text-gray-800 font-normal">{{ id }}</span></p>
+                <p class="text-indigo-500 font-bold">E-mail: <span class="text-gray-800 font-normal">{{ email }}</span></p>
             </div>
         </div>
     </main>
